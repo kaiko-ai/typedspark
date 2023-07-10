423f79f9188a60982cb561f27ddb8fea2c117dbc
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2023-07-10 19:27:22 +0200

    Add functionality to create a schema from an existing dataframe in a notebook (#109)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update test_load_table.py
    
    ---------
    
    Co-authored-by: Nanne Aben <nanneaben@Nannes-MacBook-Pro.local>
    Co-authored-by: Nanne Aben <nanneaben@nannes-mbp.home>

--- tests/_utils/test_load_table.py
+++ tests/_utils/test_load_table.py
@@ -1,8 +1,10 @@
 from typing import Literal
 
+import pytest
 from chispa.dataframe_comparer import assert_df_equality  # type: ignore
 from pyspark.sql import SparkSession
-from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import first
+from pyspark.sql.types import IntegerType, StringType
 
 from typedspark import (
     ArrayType,
@@ -16,6 +18,8 @@ from typedspark import (
 )
 from typedspark._core.datatypes import DayTimeIntervalType
 from typedspark._core.literaltype import IntervalType
+from typedspark._utils.create_dataset import create_partially_filled_dataset
+from typedspark._utils.load_table import create_schema
 
 
 class SubSchema(Schema):
@@ -26,9 +30,9 @@ class A(Schema):
     a: Column[IntegerType]
     b: Column[ArrayType[IntegerType]]
     c: Column[ArrayType[MapType[IntegerType, IntegerType]]]
-    d: Column[StructType[SubSchema]]
-    e: Column[DayTimeIntervalType[IntervalType.HOUR, IntervalType.MINUTE]]
-    f: Column[DecimalType[Literal[7], Literal[2]]]
+    d: Column[DayTimeIntervalType[IntervalType.HOUR, IntervalType.MINUTE]]
+    e: Column[DecimalType[Literal[7], Literal[2]]]
+    value_container: Column[StructType[SubSchema]]
 
 
 def test_load_table(spark: SparkSession) -> None:
@@ -51,3 +55,60 @@ def test_load_table_with_schema_name(spark: SparkSession) -> None:
     assert_df_equality(df, df_loaded)
     assert schema.get_structtype() == A.get_structtype()
     assert schema.get_schema_name() == "A"
+
+
+class B(Schema):
+    a: Column[StringType]
+    b: Column[IntegerType]
+    c: Column[StringType]
+
+
+def test_create_schema(spark: SparkSession) -> None:
+    df = (
+        create_partially_filled_dataset(
+            spark,
+            B,
+            {
+                B.a: ["a", "b!!", "c", "a", "b!!", "c", "a", "b!!", "c"],
+                B.b: [1, 1, 1, 2, 2, 2, 3, 3, 3],
+                B.c: ["alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota"],
+            },
+        )
+        .groupby(B.b)
+        .pivot(B.a.str)
+        .agg(first(B.c))
+    )
+
+    df, MySchema = create_schema(df, "B")
+
+    assert MySchema.get_schema_name() == "B"
+    assert "a" in MySchema.all_column_names()
+    assert "b__" in MySchema.all_column_names()
+    assert "c" in MySchema.all_column_names()
+
+
+def test_create_schema_with_duplicated_column_names(spark: SparkSession) -> None:
+    df = (
+        create_partially_filled_dataset(
+            spark,
+            B,
+            {
+                B.a: ["a", "b??", "c", "a", "b!!", "c", "a", "b!!", "c"],
+                B.b: [1, 1, 1, 2, 2, 2, 3, 3, 3],
+                B.c: ["alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota"],
+            },
+        )
+        .groupby(B.b)
+        .pivot(B.a.str)
+        .agg(first(B.c))
+    )
+
+    with pytest.raises(ValueError):
+        create_schema(df, "B")
+
+
+def test_name_of_structtype_schema(spark):
+    df = create_empty_dataset(spark, A)
+    df, MySchema = create_schema(df, "A")
+
+    assert MySchema.value_container.dtype.schema.get_schema_name() == "ValueContainer"
