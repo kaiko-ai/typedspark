7e5f4d951ac26c9bc2e995fc8d2864a3737cead6
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2023-08-28 15:15:02 +0200

    update

--- typedspark/_transforms/transform_to_schema.py
+++ typedspark/_transforms/transform_to_schema.py
@@ -1,6 +1,6 @@
 """Module containing functions that are related to transformations to DataSets."""
 from functools import reduce
-from typing import Dict, Optional, Type, TypeVar
+from typing import Dict, Optional, Type, TypeVar, Union
 
 from pyspark.sql import Column as SparkColumn
 from pyspark.sql import DataFrame
@@ -8,14 +8,30 @@ from pyspark.sql import DataFrame
 from typedspark._core.column import Column
 from typedspark._core.dataset import DataSet
 from typedspark._schema.schema import Schema
+from typedspark._transforms.rename_duplicate_columns import RenameDuplicateColumns
 from typedspark._transforms.utils import add_nulls_for_unspecified_columns, convert_keys_to_strings
 
 T = TypeVar("T", bound=Schema)
 
 
-def _duplicates(lst: list) -> set:
-    """Returns a set of the duplicates in the provided list."""
-    return {x for x in lst if lst.count(x) > 1}
+def _do_transformations(dataframe: DataFrame, transformations: Dict[str, SparkColumn]) -> DataFrame:
+    """Performs the transformations on the provided DataFrame."""
+    return reduce(
+        lambda acc, key: DataFrame.withColumn(acc, key, transformations[key]),
+        transformations.keys(),
+        dataframe,
+    )
+
+
+def _rename_temporary_keys_to_original_keys(
+    dataframe: DataFrame, problematic_key_mapping: Dict[str, str]
+) -> DataFrame:
+    """Renames the temporary keys back to the original keys."""
+    return reduce(
+        lambda acc, key: DataFrame.withColumnRenamed(acc, problematic_key_mapping[key], key),
+        problematic_key_mapping.keys(),
+        dataframe,
+    )
 
 
 def _identify_problematic_keys(
@@ -76,19 +92,29 @@ def transform_to_schema(
             }
         )
     """
-    _transformations = convert_keys_to_strings(transformations)
+    transform: Union[dict[str, SparkColumn], RenameDuplicateColumns]
+    transform = convert_keys_to_strings(transformations)
 
     if fill_unspecified_columns_with_nulls:
-        _transformations = add_nulls_for_unspecified_columns(
-            _transformations, schema, previously_existing_columns=dataframe.columns
-        )
+        transform = add_nulls_for_unspecified_columns(transform, schema, dataframe.columns)
 
-    temporary_key_mapping = _identify_problematic_keys(dataframe.columns, _transformations)
-    _transformations = _rename_keys_to_temporary_keys(_transformations, temporary_key_mapping)
+    transform = RenameDuplicateColumns(transform, schema, dataframe.columns)
 
     return DataSet[schema](  # type: ignore
-        dataframe.transform(_do_transformations, _transformations)
-        .drop(*temporary_key_mapping.keys())
-        .transform(_rename_temporary_keys_to_original_keys, temporary_key_mapping)
+        reduce(
+            lambda acc, key: DataFrame.withColumn(acc, key, _transformations[key]),
+            _transformations.keys(),
+            dataframe,
+        )
+        .drop(*problematic_key_mapping.keys())
+        .transform(
+            lambda df: reduce(
+                lambda acc, key: DataFrame.withColumnRenamed(
+                    acc, problematic_key_mapping[key], key
+                ),
+                problematic_key_mapping.keys(),
+                df,
+            )
+        )
         .select(*schema.all_column_names())
     )
