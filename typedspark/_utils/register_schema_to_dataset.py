fa2619e5c32f8f4f2b4d7e83efae13cea29f4dfd
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2023-10-25 13:33:19 +0200

    fix docstring

--- typedspark/_utils/register_schema_to_dataset.py
+++ typedspark/_utils/register_schema_to_dataset.py
@@ -66,9 +66,21 @@ def register_schema_to_dataset(dataframe: DataSet[T], schema: Type[T]) -> Type[T
 def register_schema_to_dataset_with_alias(
     dataframe: DataSet[T], schema: Type[T], alias: str
 ) -> Tuple[DataSet[T], Type[T]]:
-    """Helps combat column ambiguity.
+    """When dealing with self-joins, running `register_dataset_to_schema()` is not
+    enough.
 
-    For example:
+    Instead, we'll need `register_dataset_to_schema_with_alias()`, e.g.:
+
+    .. code-block:: python
+
+        class Person(Schema):
+            id: Column[IntegerType]
+            name: Column[StringType]
+
+        df_a, person_a = register_schema_to_dataset_with_alias(df, Person, alias="a")
+        df_b, person_b = register_schema_to_dataset_with_alias(df, Person, alias="b")
+
+        df_a.join(df_b, person_a.id == person_b.id)
     """
 
     class LinkedSchema(schema):  # type: ignore  # pylint: disable=missing-class-docstring
