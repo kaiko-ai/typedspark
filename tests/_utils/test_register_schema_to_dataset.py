d7dc3db3c57ace342b8180d67890757f7023862a
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2023-10-26 09:53:18 +0200

    add test to cover the counter in regfister_schema_to_dataset method

--- tests/_utils/test_register_schema_to_dataset.py
+++ tests/_utils/test_register_schema_to_dataset.py
@@ -27,5 +27,6 @@ def test_register_schema_to_dataset(spark: SparkSession):
     job = register_schema_to_dataset(df_b, Job)
 
     assert person.get_schema_name() == "Person"
+    assert hash(person.a) != hash(Person.a)
 
     df_a.join(df_b, person.a == job.a)
