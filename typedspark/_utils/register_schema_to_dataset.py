8930a2dc133f443ef6d18de39a74d69abe280a39
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2023-10-30 09:59:05 +0100

    Support self-joins in typedspark (#211)
    * Add register_schema_to_dataset_with_alias method
    
    * update documentation
    
    * import from typedspark, not from sub-packages
    
    * fix docstring

--- typedspark/_utils/register_schema_to_dataset.py
+++ typedspark/_utils/register_schema_to_dataset.py
@@ -1,6 +1,6 @@
 """Module containing functions that are related to registering schema's to DataSets."""
 import itertools
-from typing import Type, TypeVar
+from typing import Tuple, Type, TypeVar
 
 from typedspark._core.dataset import DataSet
 from typedspark._schema.schema import Schema
@@ -61,3 +61,34 @@ def register_schema_to_dataset(dataframe: DataSet[T], schema: Type[T]) -> Type[T
         _original_name = schema.get_schema_name()
 
     return LinkedSchema  # type: ignore
+
+
+def register_schema_to_dataset_with_alias(
+    dataframe: DataSet[T], schema: Type[T], alias: str
+) -> Tuple[DataSet[T], Type[T]]:
+    """When dealing with self-joins, running `register_dataset_to_schema()` is not
+    enough.
+
+    Instead, we'll need `register_dataset_to_schema_with_alias()`, e.g.:
+
+    .. code-block:: python
+
+        class Person(Schema):
+            id: Column[IntegerType]
+            name: Column[StringType]
+
+        df_a, person_a = register_schema_to_dataset_with_alias(df, Person, alias="a")
+        df_b, person_b = register_schema_to_dataset_with_alias(df, Person, alias="b")
+
+        df_a.join(df_b, person_a.id == person_b.id)
+    """
+
+    class LinkedSchema(schema):  # type: ignore  # pylint: disable=missing-class-docstring
+        _current_id = _counter()
+        _original_name = schema.get_schema_name()
+        _alias = alias
+
+    return (
+        dataframe.alias(alias),
+        LinkedSchema,  # type: ignore
+    )
