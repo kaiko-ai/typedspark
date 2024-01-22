a1a67342761ec583ee765b8c193ee4fdc1e3a1dc
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2024-01-22 08:41:52 +0100

    add html_extra_path to readthedocs configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+
+from typing import Any, List
+
 project = "typedspark"
 copyright = "2023, Nanne Aben, Marijn Valk"
 author = "Nanne Aben, Marijn Valk"
@@ -16,7 +19,7 @@ author = "Nanne Aben, Marijn Valk"
 extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme", "nbsphinx"]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns: List[Any] = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -26,3 +29,5 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 autodoc_inherit_docstrings = False
+
+html_extra_path = ["robots.txt"]
