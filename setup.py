f7b9a7b2f0de4bec47c0a521f6853202df93cfc5
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2024-04-12 17:30:22 +0200

    Update setup.py (#357)
    * Fix typo
    
    * Update README.rst
    
    * Update setup.py
    
    * Update requirements-dev.txt
    
    * Update setup.py
    
    * isort
    
    * update

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools_scm  # type: ignore
 from setuptools import find_packages, setup
 
 
@@ -25,8 +26,7 @@ setup(
     install_requires=get_requirements(),
     python_requires=">=3.9.0",
     classifiers=["Programming Language :: Python", "Typing :: Typed"],
-    version_config=True,
-    setup_requires=["setuptools-git-versioning"],
+    version=setuptools_scm.get_version(),
     package_data={"typedspark": ["py.typed"]},
     extras_require={
         "pyspark": ["pyspark"],
