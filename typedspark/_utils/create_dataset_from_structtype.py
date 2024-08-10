e96ab1a6cffce1781c4be2624c550ab7b96682fb
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2024-08-10 14:50:11 +0200

    update

--- typedspark/_utils/create_dataset_from_structtype.py
+++ typedspark/_utils/create_dataset_from_structtype.py
@@ -1,6 +1,7 @@
 """Utility functions for creating a ``Schema`` from a ``StructType``"""
 
-from typing import Dict, Literal, Optional, Type
+import re
+from typing import Annotated, Dict, Literal, Optional, Type
 
 from pyspark.sql.types import ArrayType as SparkArrayType
 from pyspark.sql.types import DataType
@@ -10,6 +11,7 @@ from pyspark.sql.types import MapType as SparkMapType
 from pyspark.sql.types import StructType as SparkStructType
 
 from typedspark._core.column import Column
+from typedspark._core.column_meta import ColumnMeta
 from typedspark._core.datatypes import (
     ArrayType,
     DayTimeIntervalType,
@@ -27,10 +29,20 @@ def create_schema_from_structtype(
     """Dynamically builds a ``Schema`` based on a ``DataFrame``'s ``StructType``"""
     type_annotations = {}
     attributes: Dict[str, None] = {}
+    column_name_mapping = _create_column_name_mapping(structtype)
+
     for column in structtype:
         name = column.name
         data_type = _extract_data_type(column.dataType, name)
-        type_annotations[name] = Column[data_type]  # type: ignore
+
+        mapped_name = column_name_mapping[name]
+        if mapped_name == name:
+            type_annotations[name] = Column[data_type]  # type: ignore
+        else:
+            type_annotations[mapped_name] = Annotated[
+                Column[data_type], ColumnMeta(external_name=name)
+            ]
+
         attributes[name] = None
 
     if not schema_name:
@@ -40,6 +52,35 @@ def create_schema_from_structtype(
     schema.__annotations__ = type_annotations
 
     return schema  # type: ignore
+
+
+def _create_column_name_mapping(structtype: SparkStructType) -> Dict[str, str]:
+    """Checks if there are duplicate columns after replacing illegal characters."""
+    mapping = {column: _replace_illegal_characters(column) for column in structtype.names}
+
+    renamed_columns = list(mapping.values())
+    duplicates = {
+        column: column_renamed
+        for column, column_renamed in mapping.items()
+        if renamed_columns.count(column_renamed) > 1
+    }
+
+    if len(duplicates) > 0:
+        raise ValueError(
+            "You're trying to dynamically generate a Schema from a DataFrame. "
+            + "However, typedspark has detected that the DataFrame contains duplicate columns "
+            + "after replacing illegal characters (e.g. whitespaces, dots, etc.).\n"
+            + "The folowing columns have lead to duplicates:\n"
+            + f"{duplicates}\n\n"
+            + "Please rename these columns in your DataFrame."
+        )
+
+    return mapping
+
+
+def _replace_illegal_characters(column_name: str) -> str:
+    """Replaces illegal characters in a column name with an underscore."""
+    return re.sub("[^A-Za-z0-9]", "_", column_name)
 
 
 def _extract_data_type(dtype: DataType, name: str) -> Type[DataType]:
