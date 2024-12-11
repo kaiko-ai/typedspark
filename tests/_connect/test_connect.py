e311b60a6c694610888f8ef85e0222e6121fe32b
Dmitrii Kochnev <kochnev@mindbox.cloud>
2024-12-11 22:32:56 +0300

    tests

--- tests/_connect/test_connect.py
+++ tests/_connect/test_connect.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import LongType
 
-from typedspark import Column, Schema, create_schema, transform_to_schema
+from typedspark import Column, Schema
 
 
 class A(Schema):
@@ -20,9 +20,3 @@ def test_spark_connect_works(sparkConnect: SparkSession):
     df = sparkConnect.createDataFrame([(14, "Tom"), (23, "Alice"), (16, "Bob")], ["age", "name"])
     assert df.count() == 3
     assert type(df).__module__ == "pyspark.sql.connect.dataframe"
-
-
-def test_transform_to_schema_works(sparkConnect: SparkSession):
-    df = sparkConnect.createDataFrame([(14, 23, 16)], ["a", "b", "c"])
-    typed_df = transform_to_schema(df, A)
-    assert typed_df.count() == 1
