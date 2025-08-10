87a92d4fdb9dd196381237b2d5d0f0d2a6f6b23a
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2025-08-10 13:18:15 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,10 @@ def spark():
     """Fixture for creating a spark session."""
     os.environ["PYSPARK_PYTHON"] = sys.executable
     os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+    os.environ.pop("SPARK_REMOTE", None)
+    os.environ.pop("PYSPARK_CONNECT_MODE_ENABLED", None)
 
-    spark = SparkSession.Builder().master("local[2]").getOrCreate()
+    SparkSession._instantiatedSession = None  # clear any existing session
+    spark = SparkSession.builder.master("local[2]").getOrCreate()
     yield spark
     spark.stop()
